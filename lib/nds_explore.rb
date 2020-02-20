235ba903176fce1a06a01e0c3393e38dc59a4366
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-20 13:33:31 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ def print_first_directors_movie_titles
   movies = first_director[:movies]
   counter = 0
   while counter < movies.length do
-  
-  
+  current_movie = movies[counter]
+  puts current_movie[counter]  
   
 end
